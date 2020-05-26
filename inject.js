063b630866f4700435a30d6d9f55978f12ffa062
Mertyn <maennel0815@gmail.com>
2020-05-26 21:12:33 +0200

    Added basic functionality. Needs refinement.

--- inject.js
+++ inject.js
@@ -5,8 +5,8 @@ function inject(func) {
     var scriptElement = document.createElement("script");
 
     var script = func.toString()                                // function to string
-    script = script.replace(/(\/\/.+(\n|\r))|\/\/\n/g, "");     // remove comments
-    script = script.replace(/\n|\t|\r/g, " ");                  // minify script
+    // script = script.replace(/(\/\/.+(\n|\r))|\/\/\n/g, "");     // remove comments
+    // script = script.replace(/\n|\t|\r/g, " ");                  // minify script
 
     scriptElement.innerHTML = `(${script})()`;
     scriptElement.classList.add("netflix-hotkeys");
@@ -51,7 +51,46 @@ function init() {
     // Setup
     // ==========================================================================================
     function setup() {
-         
+        var player = getNetflixPlayer();
+
+        function playPause() {
+            if (player.isPlaying()) player.pause();
+            else player.play();
+        }
+
+        function skip(millis) {
+            var current = player.getCurrentTime();
+            player.seek(current + millis);
+        }
+
+        // TODO: find better name
+        function setChapter(number) {
+            var duration = player.getDuration();
+            player.seek( (duration / 9) * number );
+        }
+
+        // TODO: clean this mess up
+        document.onkeyup = function(e) {
+            var key = e.key.toLowerCase();
+
+            if (key == "n" && e.shiftKey) {
+                var next = document.querySelector(".button-nfplayerNextEpisode");
+                next.click();
+            }
+
+            else if (e.keyCode >= 48 && e.keyCode <= 57) {
+                setChapter(e.keyCode - 48);
+            }
+
+            else {
+                switch(key) {
+                    case "j": skip(-5000); break;
+                    case "k": playPause(); break;
+                    case "l": skip(5000); break;
+                }
+            }
+            
+        }
     }
 
 
