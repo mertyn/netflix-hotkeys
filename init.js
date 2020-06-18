450e2de8876eefe7707f49da3dc2060528c7dc34
Mertyn <maennel0815@gmail.com>
2020-06-18 20:40:58 +0200

    Refactored and added skip intro

---
+++ init.js
@@ -0,0 +1,131 @@
+function init() {
+    // Functions for setup
+    // ==========================================================================================
+
+    // Wait for element to exist
+    function waitForElement(selector, callback) {
+        var poller = setInterval( function() {
+            var elements = document.querySelectorAll(selector);
+            if (elements.length < 1) return;
+            else {
+                clearInterval(poller);
+                callback();
+            }
+        });
+    }
+
+    function waitForNotElement(selector, callback) {
+        var poller = setInterval( function() {
+            var elements = document.querySelectorAll(selector);
+            if (elements.length > 0) return;
+            else {
+                clearInterval(poller);
+                callback();
+            }
+        });
+    }
+
+    // Get videoplayer to interact with
+    function getNetflixPlayer() {
+        var videoPlayer = window.netflix.appContext.state.playerApp.getAPI().videoPlayer;
+        var sessionId = videoPlayer.getAllPlayerSessionIds()[0];
+        
+        return videoPlayer.getVideoPlayerBySessionId(sessionId);
+    }
+
+    // Setup
+    // ==========================================================================================
+    function setup() {
+        var player = getNetflixPlayer();
+
+        function playPause() {
+            if (player.isPlaying()) player.pause();
+            else player.play();
+        }
+
+        function skip(millis) {
+            // Round current time to full seconds
+            var current = Math.round( player.getCurrentTime() / 1000 ) * 1000;
+            // Skip by specified amount
+            player.seek(current  + millis);
+        }
+
+        // TODO: find better name
+        function setChapter(number) {
+            var duration = player.getDuration();
+            player.seek( (duration / 9) * number );
+        }
+
+        function skipIntro() {
+            var skipButton = document.querySelector(".skip-credits>a>span");
+            skipButton.click();
+        }
+
+        function nextEpisode() {
+            var nextButton = document.querySelector(".button-nfplayerNextEpisode");
+            nextButton.click();
+        }
+
+        // Add commands to hotkeys
+        document.onkeypress = function(e) {
+            // Detect if player has changed and update
+            if (!player.isReady()) {
+                player = getNetflixPlayer();
+                window.nf_player = player;
+            }
+
+            // Hotkey parser
+            var key = e.key.toLowerCase();
+            var shift = e.shiftKey ? "shift+" : "";
+            var keyString = shift + key;
+
+            // Set the chapter
+            if (e.keyCode >= 48 && e.keyCode <= 57) {
+                setChapter(e.keyCode - 48);
+            }
+            else {
+                // Assign actions to hotkeys
+                switch(keyString) {
+                    case "j": skip(-5000); break;
+                    case "k": playPause(); break;
+                    case "l": skip(5000); break;
+
+                    case "shift+s": skipIntro(); break;
+                    case "shift+n": nextEpisode(); break;
+                }
+            }
+
+            // if (key == "n" && e.shiftKey) {
+            //     var next = document.querySelector(".button-nfplayerNextEpisode");
+            //     next.click();
+            // }
+
+            // else if (e.keyCode >= 48 && e.keyCode <= 57) {
+            //     setChapter(e.keyCode - 48);
+            // }
+
+            // else {
+            //     switch(key) {
+                    // case "j": skip(-5000); break;
+                    // case "k": playPause(); break;
+                    // case "l": skip(5000); break;
+            //     }
+            // }
+            
+        }
+    }
+
+    // Wait for player being loaded -> wait for .AkiraPlayer
+    waitForElement(".AkiraPlayer", function(){
+        console.log("player loaded...");
+        // Wait for player ready -> wait for spinner deletion
+        waitForNotElement(".nf-loading-spinner", function() {
+            console.log("player ready");
+            setup();
+
+            // Make player global for debug purposes
+            window.nf_player = getNetflixPlayer();
+            window.getNetflixPlayer = getNetflixPlayer;
+        });
+    });
+}
--- inject.js
+++ inject.js
@@ -1,6 +1,7 @@
-console.log("content script started");
+console.log("content script started...");
 
 // Inject function and execute it
+// This could be made to accept multiple functions for more modularity
 function inject(func) {
     var scriptElement = document.createElement("script");
 
@@ -11,109 +12,6 @@ function inject(func) {
     scriptElement.innerHTML = `(${script})()`;
     scriptElement.classList.add("netflix-hotkeys");
     document.body.appendChild(scriptElement);
-}
-
-function init() {
-    // Function definitions
-    // ==========================================================================================
-
-    // Wait for element to exist
-    function waitForElement(selector, callback) {
-        var poller = setInterval( function() {
-            var elements = document.querySelectorAll(selector);
-            if (elements.length < 1) return;
-            else {
-                clearInterval(poller);
-                callback();
-            }
-        });
-    }
-
-    function waitForNotElement(selector, callback) {
-        var poller = setInterval( function() {
-            var elements = document.querySelectorAll(selector);
-            if (elements.length > 0) return;
-            else {
-                clearInterval(poller);
-                callback();
-            }
-        });
-    }
-
-    // Get videoplayer to interact with
-    function getNetflixPlayer() {
-        var videoPlayer = window.netflix.appContext.state.playerApp.getAPI().videoPlayer;
-        var sessionId = videoPlayer.getAllPlayerSessionIds()[0];
-        
-        return videoPlayer.getVideoPlayerBySessionId(sessionId);
-    }
-
-    // Setup
-    // ==========================================================================================
-    function setup() {
-        var player = getNetflixPlayer();
-
-        function playPause() {
-            if (player.isPlaying()) player.pause();
-            else player.play();
-        }
-
-        function skip(millis) {
-            var current = player.getCurrentTime();
-            player.seek(current + millis);
-        }
-
-        // TODO: find better name
-        function setChapter(number) {
-            var duration = player.getDuration();
-            player.seek( (duration / 9) * number );
-        }
-
-        // TODO: clean this mess up
-        document.onkeyup = function(e) {
-            // Detect if player has changed and update
-            if (!player.isReady()) {
-                player = getNetflixPlayer();
-                window.nf_player = getNetflixPlayer();
-            }
-
-            var key = e.key.toLowerCase();
-
-            if (key == "n" && e.shiftKey) {
-                var next = document.querySelector(".button-nfplayerNextEpisode");
-                next.click();
-            }
-
-            else if (e.keyCode >= 48 && e.keyCode <= 57) {
-                setChapter(e.keyCode - 48);
-            }
-
-            else {
-                switch(key) {
-                    case "j": skip(-5000); break;
-                    case "k": playPause(); break;
-                    case "l": skip(5000); break;
-                }
-            }
-            
-        }
-    }
-
-
-    // TODO: make this cleaner
-    // Wait for player being loaded -> wait for .AkiraPlayer
-    waitForElement(".AkiraPlayer", function(){
-        console.log("player loaded...");
-        // Wait for player ready -> wait for spinner deletion
-        waitForNotElement(".nf-loading-spinner", function() {
-            console.log("player ready");
-            setup();
-
-            // Make player global for debug purposes
-            window.nf_player = getNetflixPlayer();
-            window.getNetflixPlayer = getNetflixPlayer;
-        });
-    });
 }
 
 inject(init);
