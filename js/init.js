7012a2b3b7cb69283748cb352fc5212e0b57aebb
Mertyn <maennel0815@gmail.com>
2020-07-02 19:47:27 +0200

    Added setOnURLChange

--- js/init.js
+++ js/init.js
@@ -22,6 +22,22 @@ function waitForNotElement(selector, callback) {
     });
 }
 
+function setOnURLChange(callback) {
+    var currentURL = location.href;
+    var poller = setInterval(function() {
+        if (location.href != currentURL) {
+            currentURL = location.href;
+            callback(location.href);
+        }
+    });
+
+    return poller;
+}
+
+function clearOnURLChange(id) {
+    clearInterval(id);
+}
+
 // Get videoplayer to interact with
 function getNetflixPlayer() {
     var videoPlayer = window.netflix.appContext.state.playerApp.getAPI().videoPlayer;
