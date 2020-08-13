f2be21b999862f4e394ce122769676b454baeee7
Mertyn <maennel0815@gmail.com>
2020-08-13 15:24:04 +0200

    Updated content.js

--- js/content.js
+++ js/content.js
@@ -20,22 +20,20 @@ function clearUrlListener(id) {
 if (location.href.match(/.+:\/\/www\.netflix\.com\/watch.+/g)) {
     chrome.runtime.sendMessage("enableBrowserAction");
 }
-else {
-    // Listen for URL change while not on netflix.com/watch
-    setUrlListener(function(url) {
-        console.log(`Href changed to ${url}`);
-    
-        // If url.match("*://www.netflix.com/watch/*")
-        // TODO: make function for matching url patterns
-        if (url.match(/.+:\/\/www\.netflix\.com\/watch.+/g)) {
-            chrome.runtime.sendMessage("enableBrowserAction");
-        }
-        else {
-            chrome.runtime.sendMessage("disableBrowserAction");
-        };
-    });
-}
 
+// Listen for URL change while not on netflix.com/watch
+setUrlListener(function(url) {
+    console.log(`Href changed to ${url}`);
+
+    // If url.match("*://www.netflix.com/watch/*")
+    // TODO: make function for matching url patterns
+    if (url.match(/.+:\/\/www\.netflix\.com\/watch.+/g)) {
+        chrome.runtime.sendMessage("enableBrowserAction");
+    }
+    else {
+        chrome.runtime.sendMessage("disableBrowserAction");
+    };
+});
 
 // Set up communication with background.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
