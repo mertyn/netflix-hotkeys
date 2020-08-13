0ff17cb24e1750f6414c77949b794617fd02e352
Mertyn <maennel0815@gmail.com>
2020-08-13 22:02:12 +0200

    Added basic player functions

--- js/init.js
+++ js/init.js
@@ -1,6 +1,30 @@
-document.arrive("video", function() {
+document.arrive(".AkiraPlayer video", function() {
     var player = new PlayerInterface();
-    player.player.pause();
-
+    
+    Mousetrap.bind("j", function() { player.skip(-5000)});
+    Mousetrap.bind("k", function() { player.playPause() });
+    Mousetrap.bind("l", function() { player.skip(5000) });
+    
+    Mousetrap.bind("home", function() { player.seekPercentage(0) });
+    Mousetrap.bind("end", function() { player.seekPercentage(100) });
+    
+    Mousetrap.bind("0", function() { player.seekPercentage(0) });
+    Mousetrap.bind("1", function() { player.seekPercentage(10) });
+    Mousetrap.bind("2", function() { player.seekPercentage(20) });
+    Mousetrap.bind("3", function() { player.seekPercentage(30) });
+    Mousetrap.bind("4", function() { player.seekPercentage(40) });
+    Mousetrap.bind("5", function() { player.seekPercentage(50) });
+    Mousetrap.bind("6", function() { player.seekPercentage(60) });
+    Mousetrap.bind("7", function() { player.seekPercentage(70) });
+    Mousetrap.bind("8", function() { player.seekPercentage(80) });
+    Mousetrap.bind("9", function() { player.seekPercentage(90) });
+    
+    Mousetrap.bind("shift+n", function() { player.nextEpisode() });
+    
+    Mousetrap.bind("shift+.", function() { player.speedUp() });
+    Mousetrap.bind("shift+,", function() { player.speedDown() });
+    
+    // Make player global for debugging
     window.player = player;
+    player.player.pause();
 });
--- js/player.js
+++ js/player.js
@@ -14,4 +14,48 @@ class PlayerInterface {
         if (this.player.isPlaying()) this.player.pause();
         else this.player.play();
     }
+
+    skip(millis) {
+        // Round current time to full seconds
+        var current = Math.round( this.player.getCurrentTime() / 1000 ) * 1000;
+        // Skip by specified amount
+        this.player.seek(current  + millis);
+    }
+
+    seekPercentage(percentage) {
+        // Seek percentage
+        var duration = this.player.getDuration();
+        this.player.seek( (duration / 100) * percentage);
+
+        // Set progressbar
+        var progressBar = document.querySelector("div.current-progress");
+        var scrubber = document.querySelector("div.scrubber-head");
+
+        progressBar.style.width = `${percentage}%`;
+        scrubber.style.left = `${percentage}%`;
+    }
+
+    nextEpisode() {
+        // Get next episode button and click it
+        var nextButton = document.querySelector(".button-nfplayerNextEpisode");
+        if (nextButton) nextButton.click();
+    }
+
+    speedUp() {
+        var speed = this.player.getPlaybackRate();
+        if (speed < 2) speed += 0.25;
+        this.player.setPlaybackRate(speed);
+
+        // Replace this with ui function
+        console.log(`${speed}x`);
+    }
+
+    speedDown() {
+        var speed = this.player.getPlaybackRate();
+        if (speed > 0.25) speed -= 0.25;
+        this.player.setPlaybackRate(speed);
+
+        // Replace this with ui function
+        console.log(`${speed}x`);
+    }
 }
